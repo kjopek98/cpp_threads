c3046932e90093ffcb6d3d2e767e8ea7c144a087
Jakub Plona <plonajakub@gmail.com>
2020-05-20 18:57:21 +0200

    Add server feature to hold state of each worker, add server player queue getter

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -1,24 +1,27 @@
 #include "ThreadPool.h"
 #include <thread>
 #include <mutex>
-#include <condition_variable>
 
-ThreadPool::ThreadPool() : nThreads(std::thread::hardware_concurrency()), terminateThreadPool(false) {
+ThreadPool::ThreadPool() : nThreads(std::thread::hardware_concurrency()),
+                           workerStates(std::thread::hardware_concurrency(), States::ServerState::Idle),
+                           terminateThreadPool(false) {
     init();
 }
 
-ThreadPool::ThreadPool(unsigned int nThreads) : nThreads(nThreads), terminateThreadPool(false) {
+ThreadPool::ThreadPool(unsigned int nThreads) : nThreads(nThreads),
+                                                workerStates(nThreads, States::ServerState::Idle),
+                                                terminateThreadPool(false) {
     init();
 }
 
 void ThreadPool::init() {
     for (int i = 0; i < nThreads; ++i) {
-        pool.emplace_back(&ThreadPool::workerLoop, this);
+        pool.emplace_back(&ThreadPool::workerLoop, this, i);
     }
 }
 
-void ThreadPool::workerLoop() {
-    std::function<void()> job;
+void ThreadPool::workerLoop(int workerID) {
+    std::pair<std::function<void()>, States::ServerState> job;
     while (true) {
         {
             std::unique_lock<std::mutex> lock(jobMutex);
@@ -31,14 +34,16 @@ void ThreadPool::workerLoop() {
                 jobs.pop();
             }
         }
-        job();
+        workerStates[workerID] = job.second;
+        job.first();
+        workerStates[workerID] = States::ServerState::Idle;
     }
 }
 
-void ThreadPool::addJob(const std::function<void()> &job) {
+void ThreadPool::addJob(const std::function<void()> &job, States::ServerState beginState) {
     {
         std::unique_lock<std::mutex> lock(jobMutex);
-        jobs.push(job);
+        jobs.emplace(job, beginState);
     }
     jobCondVar.notify_one();
 }
@@ -53,4 +58,8 @@ ThreadPool::~ThreadPool() {
     for (std::thread &worker : pool) {
         worker.join();
     }
+}
+
+const std::vector<States::ServerState> &ThreadPool::getWorkerStates() const {
+    return workerStates;
 }
