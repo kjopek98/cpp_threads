b976189a17b21766ccafcd728b4ff92e00e6cb7e
Krzysztof Jopek <krzysiek71198@gmail.com>
2020-05-25 21:13:23 +0200

    part 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <ncurses.h>
 #include <thread>
 #include <vector>
+#include <chrono>
+#include <iostream>
 #include "Server.h"
 #include "Player.h"
 #include "GameSession.h"
@@ -10,6 +12,8 @@ WINDOW * win;
 int height, width, start_y, start_x;
 std::vector< Player*> players;
 std::vector< GameSession*> gameSessions;
+std::vector<std::thread> playerThreads;
+std::vector<std::thread> gameThreads;
 
 static std::string getPlayerState(Player *player){
 
@@ -50,7 +54,30 @@ static std::string getGameState(GameSession *gameSession){
     return answer;
 }
 
-void generateWindow( std::vector<Player*> players, std::vector<GameSession*> gameSessions )
+static std::string getWorkerState(States::ServerState state){
+
+    std::string answer;
+    switch(state) {
+        case States::ServerState::Idle:
+            answer = "Idle                    ";
+            break;
+        case States::ServerState::RequestGameSession:
+            answer = "RequestGameSession      ";
+            break;
+        case States::ServerState::CancelRequestGameSession:
+            answer = "CancelRequestGameSession";
+            break;
+        case States::ServerState::RequestPlayers:
+            answer = "RequestPlayers          ";
+            break;
+        case States::ServerState::CancelRequestPlayers:
+            answer = "RequestPlayers          ";
+            break;
+    }
+    return answer;
+}
+
+void generateWindow( std::vector<Player*> players, std::vector<GameSession*> gameSessions, Server &server )
 {
 
     while(1) {
@@ -60,15 +87,42 @@ void generateWindow( std::vector<Player*> players, std::vector<GameSession*> gam
         for (auto &player : players) {
             mvwprintw(win, i, 1, "Player %s - obecny stan: %s", player->getName().c_str(),
                       getPlayerState(player).c_str());
-            wrefresh(win);
+            // wrefresh(win);
             i++;
         }
+        i++;
         for (auto &gameSession : gameSessions) {
-            mvwprintw(win, i , 1, "Gra nr %d - obecny stan: %s", gameSession->getId(),
-                    getGameState(gameSession).c_str());
-            wrefresh(win);
+            mvwprintw(win, i, 1, "Gra nr %d - obecny stan: %s", gameSession->getId(),
+                      getGameState(gameSession).c_str());
+            std::vector currentPlayers = gameSession->getCurrentPlayers();
+            i++;
+            mvwprintw(win, i, 1, "W grze:             ");
+            int y = 10;
+            for (auto &player : currentPlayers) {
+                mvwprintw(win, i, y, "%s  ", player->getName().c_str());
+                y = y + 10;
+            }
+            //refresh();
+           // wrefresh(win);
             i++;
         }
+        i++;
+        std::vector waitingPlayers = server.getWaitingPlayerQueue();
+        mvwprintw(win, i, 1, "Kolejka oczekujacych:                                    ");
+        int z = 28;
+        for (auto &player : waitingPlayers) {
+            mvwprintw(win, i, z, "%s  ", player->getName().c_str());
+            z = z + 10;
+        }
+        i= i+2;
+        std::vector workersStates = server.getWorkerStates();
+        int worker_no = 1;
+        for (auto &state : workersStates) {
+            mvwprintw(win, i, 1, "Stan workera serwera nr %d: %s", worker_no, getWorkerState(state).c_str());
+            worker_no++;
+            i++;
+        }
+        refresh();
         wrefresh(win);
     }
 
@@ -81,40 +135,45 @@ int main() {
     noecho();
     cbreak();
 
-    int yMax, xMax;
-    getmaxyx(stdscr, yMax,xMax);
+    int numberOfPlayers = 3;
+    int numberOfGames = 2;
 
-    height=15, width=xMax - 12, start_y=yMax - 20, start_x=5;
+    int yMax, xMax;
+    getmaxyx(stdscr, yMax, xMax);
+
+    height = 50, width = xMax - 12, start_y = yMax - 50, start_x = 5;
     win = newwin(height, width, start_y, start_x);
 
 
-    box(win,0,0);
+    box(win, 0, 0);
     refresh();
     wrefresh(win);
 
     Server server;
 
-    Player alice("Alice", &server);
-    players.push_back(&alice);
-    Player bob("Bob", &server);
-    players.push_back(&bob);
-    Player carl("Carl", &server);
-    players.push_back(&carl);
+    for (int i = 0; i < numberOfPlayers; i++) {
+        std::string name = std::to_string(i);
+        Player player( name , &server);
+        players.push_back(&player);
+    }
 
-    GameSession game1(1, &server);
-    gameSessions.push_back(&game1);
-    GameSession game2(2, &server);
-    gameSessions.push_back(&game2);
+    for (int i = 0; i < numberOfGames; i++) {
+        GameSession game( i, &server);
+        gameSessions.push_back(&game);
+    }
 
-    std::thread GUIThread(&generateWindow, std::ref(players), std::ref(gameSessions));
 
-    std::thread aliceThread(std::ref(alice));
-    std::thread bobThread(std::ref(bob));
-    std::thread carlThread(std::ref(carl));
+    std::thread GUIThread(&generateWindow, std::ref(players), std::ref(gameSessions), std::ref(server));
 
-    std::thread game1Thread(std::ref(game1));
-    std::thread game2Thread(std::ref(game2));
 
+
+    for (auto &player : players) {
+      playerThreads.emplace_back((std::ref(player)));
+    }
+
+    /*for (auto &player : players) {
+        std::thread newThread(std::ref(player));
+    }*/
     GUIThread.join();
     aliceThread.join();
     bobThread.join();
